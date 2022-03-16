d3fae4aa76ed8ebd67258b766ca7eafb799b90b2
tulay <tmagar448@gmail.com>
2022-03-16 16:23:57 -0400

    react-animate-on-scroll component website link

--- src/About.js
+++ src/About.js
@@ -14,6 +14,7 @@ import withReveal from 'react-reveal/withReveal';
 
 import ScrollAnimation from 'react-animate-on-scroll';
 //https://www.react-reveal.com/examples/common/lightspeed/
+//https://bestofreactjs.com/repo/dbramwell-react-animate-on-scroll
 function About() {
   return (
     <>
