35e682da4d47eed61ed523a5c7385df2a037d525
tulay <tmagar448@gmail.com>
2022-03-17 00:12:27 -0400

    added flex

--- src/Home.js
+++ src/Home.js
@@ -54,19 +54,31 @@ const Home = () => {
         
             <div className="Static-container">
 
-                <div className="flex-child magenta">
-                    Flex Column 1
+                <div className="image">
+                        Recently, Microsoft has been updating software as usual. This time Microsoft plan to update the outlook navigation panel
+                    display style to vertical panel on the left side. Before this new update, we used to see navigation panel display on the buttom left side of outlook. Now, Microsoft are letting to use some user who has turn
+                    on "Comming Soon" feature. People has "Comming Soon" feature are having confusion over this new update. You can turn off the "Comming Soon" and don't use it until new feature are
+                    permenently avaiable to everyone or get use it ahead than most people. I would suggest IT professional to use this feature as soon as possible and get familiar with the new navigation panel 
+                    display style so, you will assist other people fast and concise way.
                 </div>
 
-                <div className="flex-child green">
-                    Flex Column 2
+                <div className="image">
+                    having confusion over this new update. You can turn off the "Comming Soon" and don't use it until new feature are
+                    permenently avaiable to everyone or get use it ahead than most people. I would suggest IT professional to use this feature as soon as possible and get familiar with the new navigation panel 
+                    display style so, you will assist other people fast and concise way.
                 </div>
-                <div className="flex-child magenta">
-                    Flex Column 1
+                <div className="image">
+                        Recently, Microsoft has been updating software as usual. This time Microsoft plan to update the outlook navigation panel
+                    display style to vertical panel on the left side. Before this new update, we used to see navigation panel display on the buttom left side of outlook. Now, Microsoft are letting to use some user who has turn
+                    on "Comming Soon" feature. People has "Comming Soon" 
                 </div>
 
-                <div className="flex-child green">
-                    Flex Column 2
+                <div className="image">
+                        Recently, Microsoft has been updating software as usual. This time Microsoft plan to update the outlook navigation panel
+                    display style to vertical panel on the left side. Before this new update, we used to see navigation panel display on the buttom left side of outlook. Now, Microsoft are letting to use some user who has turn
+                    on "Comming Soon" feature. People has "Comming Soon" feature are having confusion over this new update. You can turn off the "Comming Soon" and don't use it until new feature are
+                    permenently avaiable to everyone or get use it ahead than most people. I would suggest IT professional to use this feature as soon as possible and get familiar with the new navigation panel 
+                    display style so, you will assist other people fast and concise way.
                 </div>
 
             </div>
