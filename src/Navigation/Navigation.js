223ca23ec518dd85ae0a565f2dcdd64d07aae2a5
tulay <tmagar448@gmail.com>
2022-03-19 23:01:12 -0400

    changing path value

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -16,7 +16,7 @@ function Navigation () {
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse id='responsive-navbar-nav'>
             <Nav className='me-auto'>
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/computing">Home</Nav.Link>
             </Nav>
             <Nav>
               <Nav.Link href="/about">About</Nav.Link>
