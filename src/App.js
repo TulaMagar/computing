78d335fd983b29a01c78b685f38be7fba56e8d3b
tulay <tmagar448@gmail.com>
2022-03-22 00:07:49 -0400

    refreshing on link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from './About/About.js';
 import Thop from './Shop';
 import Shop from './Questions/RouterWithBootstrap.js';
 import ItemDetails from "./ItemDetails";
-import {HashRouter, Routes, Route, Navigate} from 'react-router-dom';
+import {BrowserRouter,HashRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Footer from './Footer/Footer';
 import Routess from './Routess';
 
--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Navigation.css';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link, useNavigate, NavLink } from 'react-router-dom';
+import { Link, useNavigate, NavLink, useLocation } from 'react-router-dom';
+import {refresh} from 'react-router';
 
 //https://react-bootstrap.github.io/components/navbar/ 
 // Fix nav bar this link
 function Navigation () {
 
-  const navigate = useNavigate();
-  const reFreshPage = () => {
-    navigate(0);
+  // onUpdate={window.scrollTo(0,0)}
+  const sampleLocation = useLocation();
+  const componentDidMount = () => {
+    window.location.reload();
   }
   return (
     <>
@@ -18,11 +20,11 @@ function Navigation () {
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse className='mobile-nav' id='responsive-navbar-nav'>
           <Nav className='me-auto'>
-              <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/" onClick={() => componentDidMount()}>Home</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link as={NavLink} to="/about">About</Nav.Link>
-              <Nav.Link as={NavLink} to="/shop">Questions</Nav.Link>
+              <Nav.Link as={NavLink} to="/about" onClick={() => componentDidMount()}>About</Nav.Link>
+              <Nav.Link as={NavLink} to="/shop" onClick={() => componentDidMount()}>Questions</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
