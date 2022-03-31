5fef345ac2e5dabf9777e4461ccf0c3b86d8edb1
tulay <tmagar448@gmail.com>
2022-03-30 22:07:14 -0400

    fixed Helmet UNSAFE issue and react syntax error

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,151 +1,94 @@
-import React from 'react';
-import SlideShow from '../Slideshow/SlideShow.js';
-import './Home.css';
-import ScrollAnimation from 'react-animate-on-scroll';
-import Clean from '../Background/clean.avif';
-import Moon from '../Background/moon.webp';
-import Sea from '../Background/sea.jpg';
-import {Helmet} from "react-helmet";
-import Globe from './Globe/giphy.gif';
-import styled, { css } from 'styled-components';
+import React from "react";
+import SlideShow from "../Slideshow/SlideShow.js";
+import "./Home.css";
+import Clean from "../Background/clean.avif";
+import Moon from "../Background/moon.webp";
+import Sea from "../Background/sea.jpg";
+// import { Helmet } from "react-helmet";
+import Globe from "./Globe/giphy.gif";
+import styled, { css } from "styled-components";
 
 //Module build failed (from ./node_modules/source-map-loader/dist/cjs.js):
 
-function FadeInSection(props) {
-    const [isVisible, setVisible] = React.useState(false);
-    const domRef = React.useRef();
-    React.useEffect(() => {
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    setVisible(entry.isIntersecting);
-                }
-            });
-        });
-        observer.observe(domRef.current);
-        return () => observer.unobserve(domRef.current);
-    }, []);
-    return (
-        <div
-            className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
-            ref={domRef}
-        >
-            {props.children}
-        </div>
-    );
-}
-
-
 const Home = () => {
+  return (
+    <React.Fragment>
+      {/* <Helmet>
+        <meta charSet="utf-8" />
+        <title>computing</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet> */}
 
-    // const FadeIn = styled.div `
-    //     opacity: ${( showPopup ) => (showPopup ? '1' : '0')};
-        
-    // `;
-    
-    return (
-        <React.Fragment>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>computing</title>
-                <link rel="canonical" href="http://mysite.com/example" />
-            </Helmet>
+      <SlideShow />
 
-            <ScrollAnimation animateIn='fadeIn'
-                    animateOut='fadeOut'>
-            
-                <SlideShow/>
+      <div className="container-large">
+        <div className="flex-container">
+          <div className="flex-child magenta">Flex Column 1</div>
 
-            </ScrollAnimation>
+          <div className="flex-child green">Flex Column 2</div>
+          <div className="flex-child magenta">Flex Column 1</div>
 
-            <FadeInSection>
-                <div className='container-large'>
-                    <div className="flex-container">
+          <div className="flex-child green">Flex Column 2</div>
+        </div>
+      </div>
 
-                        <div className="flex-child magenta">
-                            Flex Column 1
-                        </div>
-                        
-                        <div className="flex-child green">
-                            Flex Column 2
-                        </div>
-                        <div className="flex-child magenta">
-                            Flex Column 1
-                        </div>
-                        
-                        <div className="flex-child green">
-                            Flex Column 2
-                        </div>
-            
-                    </div>
-                </div>
-            </FadeInSection>
+      <p className="context">
+        <span>
+          Find the similar issues and solution.{" "}
+          <strong>
+            {" "}
+            Not avaiable: Right now, posting a issue isn't avaiable but I will
+            build it soon!{" "}
+          </strong>
+        </span>
+      </p>
 
-            <ScrollAnimation animateIn='fadeIn'
-                    animateOut='fadeOut'>
-            
-                <p className='context'>
-                    <span> 
-                        Find the similar issues and solution. <strong> Not avaiable: Right now, posting a issue isn't avaiable but 
-                        I will build it soon! </strong>
-                    </span>
-                </p>
-            </ScrollAnimation>
+      <div className="container-L-image">
+        <div className="Static-container">
+          <div className="image">
+            <img src={Clean} alt="Clean not found" />
+            <p>
+              <span> </span>
+            </p>
+          </div>
 
-            <FadeInSection>
-            
-                <div className='container-L-image'>
-                        <div className="Static-container">
-                            
-                            <div className="image">
-                                <img src={Clean} alt="Clean not found"/>
-                                <p>
-                                    <span> </span>
-                                </p>
-                            </div>
+          <div className="image">
+            <img src={Moon} alt="Clean not found" />
+          </div>
+          <div className="image">
+            <img src={Sea} alt="Clean not found" />
+          </div>
 
-                            <div className="image">
-                                <img src={Moon} alt="Clean not found"/>
-                            </div>
-                            <div className="image">
-                                <img src={Sea} alt="Clean not found"/>
-                            </div>
+          <div className="image">
+            <img src={Clean} alt="Clean not found" />
+          </div>
+        </div>
+      </div>
 
-                            <div className="image">
-                                <img src={Clean} alt="Clean not found"/>
-                            </div>
+      <div className="container-ReachOut">
+        <div className="ReachOut-container">
+          <div className="side">
+            <p>
+              <span>
+                Find the similar issues and solution.{" "}
+                <strong>
+                  {" "}
+                  Not avaiable: Right now, posting a issue isn't avaiable but I
+                  will build it soon!{" "}
+                </strong>{" "}
+                ind the similar issues and solution. Not avaiable: Right now,
+                posting a issue isn't avaiable but I will build it soon!
+              </span>
+            </p>
+          </div>
 
-                        </div>
-                    </div>
-            </FadeInSection>
- 
-            <ScrollAnimation animateIn='fadeIn'
-                    animateOut='fadeOut'>
-                    
-                    <div className='container-ReachOut'>
-                        <div className="ReachOut-container">
-                            
-                            <div className="side">
-                                <p>
-                                    <span> 
-                                        Find the similar issues and solution. <strong> Not avaiable: Right now, posting a issue isn't avaiable but 
-                                        I will build it soon! </strong> ind the similar issues and solution. Not avaiable: Right now, posting a issue isn't avaiable but 
-                                        I will build it soon!
-                                    </span>
-                                </p>
-                            </div>
-
-                            <div className="side">
-                                <img src={Globe} alt="globe not found"/>
-                            </div>
-
-                        </div>
-                    </div>
-
-            </ScrollAnimation>
-        </React.Fragment>
-    );
-
-}
+          <div className="side">
+            <img src={Globe} alt="globe not found" />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Home;
--- src/Questions/RouterWithBootstrap.js
+++ src/Questions/RouterWithBootstrap.js
@@ -1,83 +1,170 @@
-import React, { useState, useEffect } from 'react';
-import './RouterWithBootstrap.css';
-import {Helmet} from "react-helmet";
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./RouterWithBootstrap.css";
+// import {Helmet} from "react-helmet";
+import { Link } from "react-router-dom";
 
 //https://codesandbox.io/s/admiring-kalam-lpxwel?file=/src/Navigation.js
 
-function RouterWithBootstrap () {
+function RouterWithBootstrap() {
+  const textInput = React.createRef(); // React use ref to get input value
 
-    const textInput = React.createRef();  // React use ref to get input value
-    
+  const datas = [
+    { url: "test", description: "this is this website" },
+    { url: "test", description: "../About/About.js" },
+    { url: "test", description: "this is this website" },
+    { url: "test", description: "this is that website" },
+    { url: "test", description: "this is this website" },
+    { url: "test", description: "this is that website" },
+    { url: "test", description: "this is this website" },
+    { url: "test", description: "this is that website" },
+    { url: "test", description: "this is this website" },
+    { url: "test", description: "this is that website" },
+    { url: "test", description: "this is this website" },
+    { url: "test", description: "this is that website" },
+    { url: "test", description: "this is this website" },
+    { url: "test", description: "this is that website" },
+  ];
 
-    const datas = [{url:"test", description: "this is this website"}, 
-        {url:"test", description: "../About/About.js"},
-        {url:"test", description: "this is this website"}, 
-        {url:"test", description: "this is that website"},
-        {url:"test", description: "this is this website"}, 
-        {url:"test", description: "this is that website"},
-        {url:"test", description: "this is this website"}, 
-        {url:"test", description: "this is that website"},
-        {url:"test", description: "this is this website"}, 
-        {url:"test", description: "this is that website"},
-        {url:"test", description: "this is this website"}, 
-        {url:"test", description: "this is that website"},
-        {url:"test", description: "this is this website"}, 
-        {url:"test", description: "this is that website"}]
+  const fruit = [
+    ["apple", "banana"],
+    "orange",
+    "grapefruit",
+    "mango",
+    "strawberry",
+    "peach",
+    "apricot",
+  ];
 
-    const fruit = [['apple', 'banana'], 'orange', 'grapefruit',
-    'mango', 'strawberry', 'peach', 'apricot'];
+  const [filter, setFilter] = useState("");
 
-    const [filter, setFilter] = useState('');
-
-    useEffect(() => {
-        
-    })
-    return (
-        <>
-            <Helmet>
+  useEffect(() => {});
+  return (
+    <>
+      {/* <Helmet>
                 <meta charSet="utf-8" />
                 <title>questions</title>
-                {/* <link rel="canonical" href="http://mysite.com/example" /> */}
-            </Helmet>
-            <div id="container">
-                <div id="left">
-                    <h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1>
-                </div>
-        
-                <div id="middle">
+                <link rel="canonical" href="http://mysite.com/example" />
+            </Helmet> */}
 
-                        <p className='input-container'>
-                            <input id="filter"
-                                className='input'
-                                name="filter"
-                                type="text"
-                                value={filter}
-                                placeholder="type to search for similar question and answer"
-                                onChange={event => setFilter(event.target.value)}
-                            />
-                        </p>
-                        <ul className='question-list'>
-                            {datas.filter(f => f.description.toString().toLowerCase().includes(filter.toString().toLowerCase()) || filter === '')
-                                .map((f, index) => 
-                                    <div key={index}>
-                                        <li> {f.url} </li>
-                                        {/* <Link to={`/shop/${index}`}>{f.description}</Link> */}
-                                        <Link to={f.description}>{f.description}</Link>
-                                    </div>
-                                )} 
-                        </ul>
-                </div>
-        
-                <div id="right">
-                    <h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br /><h1>RIGHT SIDE</h1><br />
-                </div>
-            </div>
-        </>
+      <div id="container">
+        <div id="left">
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+        </div>
 
+        <div id="middle">
+          <p className="input-container">
+            <input
+              id="filter"
+              className="input"
+              name="filter"
+              type="text"
+              value={filter}
+              placeholder="type to search for similar question and answer"
+              onChange={(event) => setFilter(event.target.value)}
+            />
+          </p>
+          <ul className="question-list">
+            {datas
+              .filter(
+                (f) =>
+                  f.description
+                    .toString()
+                    .toLowerCase()
+                    .includes(filter.toString().toLowerCase()) || filter === ""
+              )
+              .map((f, index) => (
+                <div key={index}>
+                  <li> {f.url} </li>
+                  {/* <Link to={`/shop/${index}`}>{f.description}</Link> */}
+                  <Link to={f.description}>{f.description}</Link>
+                </div>
+              ))}
+          </ul>
+        </div>
 
-    );
-    
+        <div id="right">
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+          <h1>RIGHT SIDE</h1>
+          <br />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default RouterWithBootstrap;
