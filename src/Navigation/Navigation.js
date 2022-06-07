af3b92f18991861f91174d59d5a06d8f20dbf322
tulay <tmagar448@gmail.com>
2022-06-06 23:09:25 -0400

    // eslint-disable-next-line

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -3,12 +3,14 @@ import "./Navigation.css";
 import { Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/js/bootstrap.bundle";
 import { NavLink } from "react-router-dom";
+// eslint-disable-next-line
 import { GoogleLogin, GoogleLogout } from "react-google-login";
 
 //https://stackoverflow.com/questions/40491483/hide-collapse-navbar-after-link-click-react-redux-react-bootstrap
 
 function Navigation() {
   const [expanded, setExpanded] = useState(false);
+  // eslint-disable-next-line
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
