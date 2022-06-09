9cda2a332166e5abfd21067a4cb5b27995e8aec5
tulay <tmagar448@gmail.com>
2022-06-09 19:45:40 -0400

    style changed for programming languages

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,40 +1,35 @@
 import React, { useRef } from "react";
 import Slideshow from "../Slideshow/SlideShow.js";
 import "./Home.css";
-import Moon from "../Background/moon.webp";
-import Sea from "../Background/sea.jpg";
+// import Moon from "../Background/moon.webp";
+// import Sea from "../Background/sea.jpg";
 import "animate.css/animate.min.css";
 import UseTitle from "../Title.js";
 import { Link } from "react-router-dom";
 const Home = () => {
-  const [ Ref, Ref2, Ref3, Ref4] = [
-    useRef(),
-    useRef(),
-    useRef(),
-    useRef(),
-  ];
+  const [Ref, Ref2, Ref3, Ref4] = [useRef(), useRef(), useRef(), useRef()];
 
   const MoveToBenefits = () =>
     window.scrollTo({
-      top: Ref.current.offsetTop - 115,
+      top: Ref4.current.offsetTop - 115,
       behavior: "smooth",
     });
 
-  const MoveToTeam = () =>
+  const MoveToLanguages = () =>
     window.scrollTo({
-      top: Ref2.current.offsetTop - 115,
+      top: Ref3.current.offsetTop - 97,
       behavior: "smooth",
     });
 
   const MoveToMission = () =>
     window.scrollTo({
-      top: Ref3.current.offsetTop - 115,
+      top: Ref2.current.offsetTop - 115,
       behavior: "smooth",
     });
 
   const Move = () =>
     window.scrollTo({
-      top: Ref4.current.offsetTop - 115,
+      top: Ref.current.offsetTop - 115,
       behavior: "smooth",
     });
 
@@ -48,11 +43,11 @@ const Home = () => {
           <button className="flex-child" onClick={MoveToBenefits}>
             Move To Benefits
           </button>
-          <button className="flex-child" onClick={MoveToTeam}>
-            Move To Team
-          </button>
-          <button className="flex-child" onClick={MoveToMission}> 
+          <button className="flex-child" onClick={MoveToMission}>
             Move To Mission
+          </button>
+          <button className="flex-child" onClick={MoveToLanguages}>
+            Move To Languages
           </button>
           <button className="flex-child" onClick={Move}>
             Move To Gain
@@ -92,8 +87,122 @@ const Home = () => {
           </p>
         </div>
       </div>
-
-      <div className="container-L-image">
+      <div className="bg_programming">
+        <div className="programming_name" ref={Ref3}>
+          <div className="html">
+            <h1>HTML</h1>
+            <p>
+              HTML stands for Hypertext Markup Language. It is the language used
+              to create web pages.
+            </p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="css">
+            <h1>CSS</h1>
+            <p>
+              CSS stands for Cascading Style Sheets. It is a style sheet
+              language used to make the web pages look nice.
+            </p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="javascript">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="python">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="reactjs">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="nodejs">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="csharp">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="java">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="cplusplus">
+            <h1>lorem</h1>
+            <p>What is its?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="sql">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="php">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="ruby">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;
+            </Link>
+          </div>
+          <div className="git">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;{" "}
+            </Link>
+          </div>
+          <div className="windows">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;{" "}
+            </Link>
+          </div>
+          <div className="linux">
+            <h1>lorem</h1>
+            <p>What is it?</p>
+            <Link className="different_site" to="/contact">
+              Learn More &#x2192;{" "}
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* <div className="container-L-image">
         <div className="Static-container">
           <div className="image">
             <img loading="lazy" src={Sea} alt="Clean not found" />
@@ -110,9 +219,9 @@ const Home = () => {
             <img loading="lazy" src={Moon} alt="Clean not found" />
           </div>
         </div>
-      </div>
+      </div> */}
 
-      <div className="Benefit_bg">
+      <div className="Benefit_bg" ref={Ref4}>
         <div className="whatWeDo">
           <h1 className="WhatWeDoInfoHeader">Benefits</h1>
           <p className="WhatWeDoInfo">
@@ -145,67 +254,6 @@ const Home = () => {
           </p>
         </div>
       </div>
-
-
-
-
-      <div className="programming_name">
-        <div className="html"> 
-          <h1>lorem</h1>
-          <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div className="css"><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div className="javascript"><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div className="python"><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div className="react"><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div className="node"><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div className="cshard"><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div className="java"><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div className="cplusplus"><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192;</Link>
-         </div>
-        <div><h1>lorem</h1>
-        <p>What is it?</p>
-          <Link className="different_site" to="/contact">Learn More to Different Site &#x2192; </Link>
-         </div>
-      </div>
-
-
     </React.Fragment>
   );
 };
