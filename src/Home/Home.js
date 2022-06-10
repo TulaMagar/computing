f6200f65d7aa9447143430f1c81c874da47de812
tulay <tmagar448@gmail.com>
2022-06-09 21:05:15 -0400

    links to within page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,8 @@
 import React, { useRef } from "react";
 import Slideshow from "../Slideshow/SlideShow.js";
 import "./Home.css";
-// import Moon from "../Background/moon.webp";
-// import Sea from "../Background/sea.jpg";
+import Moon from "../Background/moon.webp";
+import Sea from "../Background/sea.jpg";
 import "animate.css/animate.min.css";
 import UseTitle from "../Title.js";
 import { Link } from "react-router-dom";
@@ -17,19 +17,19 @@ const Home = () => {
 
   const MoveToLanguages = () =>
     window.scrollTo({
-      top: Ref3.current.offsetTop - 97,
+      top: Ref3.current.offsetTop - 84,
       behavior: "smooth",
     });
 
-  const MoveToMission = () =>
+  const MoveToNonRelated = () =>
     window.scrollTo({
       top: Ref2.current.offsetTop - 115,
       behavior: "smooth",
     });
 
-  const Move = () =>
+  const MoveToFooter = () =>
     window.scrollTo({
-      top: Ref.current.offsetTop - 115,
+      top: Ref.current.offsetTop - 100,
       behavior: "smooth",
     });
 
@@ -43,14 +43,14 @@ const Home = () => {
           <button className="flex-child" onClick={MoveToBenefits}>
             Move To Benefits
           </button>
-          <button className="flex-child" onClick={MoveToMission}>
-            Move To Mission
+          <button className="flex-child" onClick={MoveToNonRelated}>
+            Move To Non Related
           </button>
           <button className="flex-child" onClick={MoveToLanguages}>
             Move To Languages
           </button>
-          <button className="flex-child" onClick={Move}>
-            Move To Gain
+          <button className="flex-child" onClick={MoveToFooter}>
+            Move To Footer
           </button>
         </div>
       </div>
@@ -93,7 +93,7 @@ const Home = () => {
             <h1>HTML</h1>
             <p>
               HTML stands for Hypertext Markup Language. It is the language used
-              to create web pages.
+              to create web pages ....
             </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
@@ -103,106 +103,147 @@ const Home = () => {
             <h1>CSS</h1>
             <p>
               CSS stands for Cascading Style Sheets. It is a style sheet
-              language used to make the web pages look nice.
+              language used to make the web pages look nice ....
             </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="javascript">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>JavaScript</h1>
+            <p>
+              JavaScript is a interpreted programming language. Used for web,
+              mobile and desktop applications ....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="python">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>Python</h1>
+            <p>
+              Python is a interpreted programming language. Used for developing
+              web applications and software ....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="reactjs">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>React js</h1>
+            <p>
+              React js is a JavaScript library for building user interfaces for
+              web ....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="nodejs">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>Node js</h1>
+            <p>
+              Node js is a JavaScript runtime environment for executing
+              JavaScript.
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="csharp">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>C#</h1>
+            <p>
+              C# is a object oriented programming language. Used to develop
+              desktop and web applications ....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="java">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>Java</h1>
+            <p>
+              Java is a object oriented programming language. Used to develop
+              desktop and web applications ....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="cplusplus">
-            <h1>lorem</h1>
-            <p>What is its?</p>
+            <h1>C++</h1>
+            <p>
+              C++ is a object oriented programming language. Used to develop
+              desktop and web applications ....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="sql">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>SQL</h1>
+            <p>
+              SQL is a database management system. Used to store and manage data
+              ....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="php">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>PHP</h1>
+            <p>
+              PHP is a server-side scripting language. Used to develop web and
+              ....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="ruby">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>Ruby</h1>
+            <p>
+              Ruby is a object oriented programming language. Used to develop
+              .....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;
             </Link>
           </div>
           <div className="git">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>Git</h1>
+            <p>
+              Git is a version control system. Used to manage the any project
+              and ....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;{" "}
             </Link>
           </div>
           <div className="windows">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>Windows</h1>
+            <p>
+              Windows is a operating system by Microsoft. Operating system is
+              what is used to run the computer and ....
+            </p>
+
             <Link className="different_site" to="/contact">
               Learn More &#x2192;{" "}
             </Link>
           </div>
           <div className="linux">
-            <h1>lorem</h1>
-            <p>What is it?</p>
+            <h1>Linux</h1>
+            <p>
+              Linux is a open source operating system. You can alter the kernel
+              of the linux operating system and ....
+            </p>
             <Link className="different_site" to="/contact">
               Learn More &#x2192;{" "}
             </Link>
           </div>
         </div>
       </div>
-      {/* <div className="container-L-image">
+
+      <div className="container-L-image" ref={Ref2}>
         <div className="Static-container">
           <div className="image">
             <img loading="lazy" src={Sea} alt="Clean not found" />
@@ -219,7 +260,7 @@ const Home = () => {
             <img loading="lazy" src={Moon} alt="Clean not found" />
           </div>
         </div>
-      </div> */}
+      </div>
 
       <div className="Benefit_bg" ref={Ref4}>
         <div className="whatWeDo">
@@ -254,6 +295,7 @@ const Home = () => {
           </p>
         </div>
       </div>
+      <span ref={Ref}></span>
     </React.Fragment>
   );
 };
