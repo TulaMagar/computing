749018a74df03e39c2e027fc8c040991f383fcef
tulay <tmagar448@gmail.com>
2022-06-14 15:53:22 -0400

    html intro not yet done

--- src/programming_languages/html_l/Html_Intro.js
+++ src/programming_languages/html_l/Html_Intro.js
@@ -27,13 +27,18 @@ export default function html_intro() {
 
         <div id="middle">
           <h3>What is HTML?</h3>
-          <p>
-            HTML is the standard markup language for creating Web pages. HTML is used to create pages that are displayed in a Web browser. 
-            HTML is a markup language, which is a way to write instructions for a computer to format text, images, and other objects on a Web page.
-          </p>
+
+          <ol>
+            <li>HTML is a markup language.</li>
+            <li>HTML is used to create pages that are displayed in a Web browser.</li>
+            <li>HTML is a way to write instructions for a computer to format text, images, and other objects on a Web page.</li>
+          </ol>
           <p>
             HTML is the easiest language to learn, and is the most basic language for creating Web pages.
-            .
+            You don't need to use lots of logic to use HTML but you will need to know how to use it. 
+            There are so many HTMl elements that you will need to know how to use them. 
+            In this course you will learn how to use HTML and how to create your web pages in a simple to complex way. 
+            So, you will be able to create your own web pages. If you complete this course you will master the HTML language. Bye Bye HTML!
           </p>
 
         </div>
--- src/programming_languages/html_l/Html_home.js
+++ src/programming_languages/html_l/Html_home.js
@@ -23,7 +23,37 @@ export default function Html_Home() {
           <h1>RIGHTTT SIDE7</h1>
         </div>
 
-        <div id="middle">Welcome to the HTML home page.</div>
+        <div id="middle">
+          <h3> Welcome to the HTML home page.</h3>
+          
+          <p>
+            This is the home page for the HTML programming language. In this page, you can find information about the language, its syntax, and its history.
+          </p>
+
+          <h3>What is HTML?</h3>
+
+          <ol>
+            <li>HTML is a markup language.</li>
+            <li>HTML is used to create pages that are displayed in a Web browser.</li>
+            <li>HTML is a way to write instructions for a computer to format text, images, and other objects on a Web page.</li>
+          </ol>
+          
+          <h3>HTMl History</h3>
+          <ol>
+           <li>
+              HTML 1.0 was released in 1993 with the intention of sharing information that can be readable and accessible via web browsers. But not many of the developers were involved in creating websites. So the language was also not growing.
+
+              </li><li> 
+Then comes HTML 2.0, published in 1995, which contains all the features of HTML 1.0 along with that few additional features, which remained as the standard markup language for designing and creating websites until January 1997 and refined various core features of HTML.
+</li>
+<li>Then comes HTML 3.0, where Dave Raggett introduced a fresh paper or draft on HTML. It included improved new features of HTML, giving more powerful characteristics for webmasters in designing web pages. But these powerful features of the new HTML slowed down the browser in applying further improvements.</li>
+<li>Then comes HTML 4.01, which is widely used and was a successful version of HTML before HTML 5.0, which is currently released and used worldwide. HTML 5 can be said for an extended version of HTML 4.01, which was published in the year 2012.
+
+           </li>
+          </ol>
+          
+        </div>
+      
 
         <div id="right">
           <RelatedLinks />
